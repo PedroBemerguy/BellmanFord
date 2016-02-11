6e246be1b9fc26b725ea0077c2386e276b79140b
Pedro Bemerguy <pedrobemerguy96@gmail.com>
2016-02-11 12:00:08 -0300

    Optimize code and  implement style PEP 08 guide

--- bellman-ford.py
+++ bellman-ford.py
@@ -1,57 +1,60 @@
 import argparse
-import numpy
-
+import string
 ## This code implement Bellman-Ford Algorithm
 
-## We use a class called bellmanFord reponsible to apply and verify the relaxation
-##technique and negative-weight cycle,respectively.
+## We use a class called bellmanFord reponsible to apply and verify the 
+## relaxation technique and negative-weight cycle,respectively.
 
 
 class bellmanFord:
-    def __init__(self,start):
-        self.graph = { 'A': { 'B': 6 , 'C': 7 }, 
-                       'B': { 'C': 8 , 'D': 5 , 'E':-4 }  , 
-                       'C': { 'D': -3 , 'E': 9 }  , 
-                       'D': { 'B': -2 }, 
-                       'E': { 'A': 2  }  
-                     }
+    def __init__(self,start,graph):
+        self.graph = graph
         self.value = {}
         self.value[start]= 0
-        for i in ['B','C','D','E']:
+        self.start = start
+        self.stringGraph = list(string.ascii_uppercase[:len(self.graph)])
+        for i in self.stringGraph[1:]:
                self.value[i] = 'inf'
-    
-
-    def relax(self,graph,start,value):
-        for interaction in xrange(1,len(graph)-1):
-            for node in ['A','B','C','D','E']:
-              self.path = graph[node].keys()
-              for k in self.path:
-                  if value[node] == 'inf':
-                    break
-                  elif value[k] == 'inf':
-                    value[k] = graph[node].get(k) + value[node]
-                  elif graph[node].get(k) + value[node] < value[k]:
-                    value[k] = graph[node].get(k) + value[node]
+        self.relax()
+        self.negativeCycle()
 
 
-    def negativeCycle(self,graph,value):                  
-        for node in ['A','B','C','D','E']: 
-          self.path = graph[node].keys()
-          for k in self.path:
-            if graph[node].get(k) + value[node] < value[k]:
-               print 'The Graph has a negative-weight cycle'
+    def relax(self):
+        for interaction in xrange(1,len(self.graph)-1):
+            for node in self.stringGraph:
+                self.path = self.graph[node].keys()
+                for k in self.path:
+                    if self.value[node] == 'inf':
+                        break
+                    elif self.value[k] == 'inf':
+                        self.value[k] = self.graph[node].get(k) + \
+                                        self.value[node]
+                    elif ( self.graph[node].get(k) + \
+                        self.value[node] < self.value[k] ):
+                        self.value[k] = self.graph[node].get(k) + \
+                                        self.value[node]
+        print self.value
+
+    def negativeCycle(self):                  
+        for node in self.stringGraph: 
+            self.path = self.graph[node].keys()
+            for k in self.path:
+                if self.graph[node].get(k) + self.value[node] < self.value[k]:
+                    print 'The Graph has a negative-weight cycle'
             
 
 
 if __name__ == '__main__':
-
-  parser = argparse.ArgumentParser()
-  parser.add_argument('-n', '--node', type=str, required=True,
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--node', type=str, required=True, \
                         help='Beginning of the node search')
-  args = parser.parse_args()
+    args = parser.parse_args()
 
-  a = bellmanFord(args.node)
-  a.relax(a.graph,args.node,a.value)
-  a.negativeCycle(a.graph,a.value)
+    bellmanFord(args.node,{ 'A': { 'B': 6 , 'C': 7 }, 
+                            'B': { 'C': 8 , 'D': 5 , 'E':-4 }  , 
+                            'C': { 'D': -3 , 'E': 9 }  , 
+                            'D': { 'B': -2 }, 
+                            'E': { 'A': 2  }  
+                          }
+               )
 
-  print a.value
